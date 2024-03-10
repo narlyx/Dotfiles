e5154c3f62219ee63203a9805898cb98f988a2c0
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-03-10 11:36:09 -0600

    Feat: Neovim adjustments

---
+++ .config/nvim/lua/set.lua
@@ -0,0 +1,9 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.bo.softtabstop = 2
+
+vim.opt.nu = true;
+vim.opt.relativenumber = true;
+
+vim.cmd.colorscheme "catppuccin-mocha"
