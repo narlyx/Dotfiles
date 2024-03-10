6d220650291962b00afcc1e78c73753327bd562c
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-03-09 22:25:35 -0700

    Feat: Better nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-vim.wo.number = true
+
+vim.opt.nu = true;
+vim.opt.relativenumber = true;
 
 -- Lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -20,50 +22,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
--- Catppuccin
-require("catppuccin").setup({
-    flavour = "frappe", -- latte, frappe, macchiato, mocha
-    background = {
-        light = "latte",
-        dark = "mocha",
-    },
-    transparent_background = true,
-    show_end_of_buffer = false,
-    term_colors = false,
-    dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-    },
-    no_italic = false,
-    no_bold = false,
-    no_underline = false,
-    styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-    },
-    color_overrides = {},
-    custom_highlights = {},
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = false,
-        mini = {
-            enabled = true,
-            indentscope_color = "",
-        },
-    },
-})
-vim.cmd.colorscheme "catppuccin"
+-- Setting Theme
+vim.cmd.colorscheme "catppuccin-mocha"
