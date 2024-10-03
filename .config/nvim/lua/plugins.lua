8569a194ded22accb9a3b3dbf815544d8ed9bad7
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-10-03 13:18:34 -0600

    Feat: added nvim-tree to neovim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -117,6 +117,24 @@ local vimTmux = {
   },
 }
 
+local nvimTree = {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("nvim-tree").setup({
+      actions = {
+        open_file = {
+          resize_window = false,
+        },
+      },
+    })
+  end,
+  keys = {
+    { "<C-n>", function() vim.cmd([[NvimTreeToggle]]) end, mode = { "n", "t" }, desc = "Toggle nvim-tree" },
+    { "<C-f>", function() vim.cmd([[NvimTreeFindFile]]) end, desc = "Show current file in nvim-tree" },
+  },
+}
+
 local catppuccinTheme = {
   "catppuccin/nvim",
   name = "catppuccin",
@@ -133,5 +151,6 @@ return {
   treesitter,
   lspZero,
   vimTmux,
-  catppuccinTheme
+  catppuccinTheme,
+  nvimTree
 }
