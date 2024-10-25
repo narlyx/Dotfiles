01bf0eb6cd17c76d03425894ec8bc1d0f2b8f52b
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-10-24 23:45:45 -0600

    Feat: NeoVim changes

---
+++ .config/nvim/lua/narlyx/plugins/barbar.lua
@@ -0,0 +1,32 @@
+return {
+  'romgrk/barbar.nvim',
+  dependencies = {
+    'lewis6991/gitsigns.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  init = function()
+    vim.g.barbar_auto_setup = true
+
+    local map = vim.api.nvim_set_keymap
+    local opts = { noremap = true, silent = true }
+
+    -- Arrow switching
+    map('n', '<A-,>', '<Cmd>BufferPrevious<CR>', opts)
+    map('n', '<A-.>', '<Cmd>BufferNext<CR>', opts)
+    -- Reorder using shift and arrows
+    map('n', '<A-<>', '<Cmd>BufferMovePrevious<CR>', opts)
+    map('n', '<A->>', '<Cmd>BufferMoveNext<CR>', opts)
+    -- Number selecting
+    map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', opts)
+    map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', opts)
+    map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', opts)
+    map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', opts)
+    map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', opts)
+    map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', opts)
+    map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', opts)
+    map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', opts)
+    map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', opts)
+    -- Close buffer
+    map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
+  end,
+}
