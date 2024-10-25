639ea6137083d5c737f2ea2327ebadf8e22f1e05
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-10-24 23:45:45 -0600

    Feat: NeoVim changes

--- .config/nvim/lua/narlyx/core/set.lua
+++ .config/nvim/lua/narlyx/core/set.lua
@@ -7,3 +7,10 @@ vim.bo.softtabstop = 2
 -- Numbering
 vim.opt.nu = true;
 vim.opt.relativenumber = true;
+
+-- Disable built in completion
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_python3_provider = 0
