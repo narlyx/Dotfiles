9084127f25c60deb97f5c95379704c370912d518
Narlyx <109835029+narlyx@users.noreply.github.com>
2024-12-12 16:16:34 -0700

    Feat: Darwin keybinds

--- .config/nvim/lua/narlyx/core/keymaps.lua
+++ .config/nvim/lua/narlyx/core/keymaps.lua
@@ -5,6 +5,10 @@ vim.keymap.set('i', '<C-v>', '<C-r>+')
 
 -- Save and quit
 vim.keymap.set('n', '<A-s>', '<cmd>w<cr>')
-vim.keymap.set('n', '<A-S>', '<cmd>wq<cr>')
 vim.keymap.set('n', '<A-q>', '<cmd>q<cr>')
 vim.keymap.set('n', '<A-Q>', '<cmd>qa!<cr>')
+
+-- Alternate save and quit for darwin
+vim.keymap.set('n', 'ß', '<cmd>w<cr>')
+vim.keymap.set('n', 'œ', '<cmd>q<cr>')
+vim.keymap.set('n', 'Œ', '<cmd>qa!<cr>')
